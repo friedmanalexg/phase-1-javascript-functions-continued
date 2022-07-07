2feedc96b62e131f2a5869d8edc6191308909075
Alex <friedmanalexg@gmail.com>
2022-07-07 17:25:44 -0400

    commit tree 36121472941c7f5dbe695e7001d97e2ced75b6fd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return(`This Saturday, I want to ${activity}!`);
+
+}
+saturdayFun()
+
+const mondayWork = function(boringActivity = "go to the office" ) {
+    return(`This Monday, I will ${boringActivity}.`)
+}
+mondayWork()
+
+function wrapAdjective(whateverYouLike = '*') {
+    return function inner(howeverYouWish = 'special'){
+        return(`You are ${whateverYouLike}${howeverYouWish}${whateverYouLike}!`)
+    }
+
+}
